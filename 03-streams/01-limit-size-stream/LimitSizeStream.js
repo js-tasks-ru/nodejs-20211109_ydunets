24fa121bb74de1f1d15f641871ff91a8feff8e3f
Yury Dunets <86290237+ydunetsdev@users.noreply.github.com>
2021-11-30 16:42:10 +0300

    node-course: add first task

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -1,14 +1,20 @@
-const stream = require('stream');
-const LimitExceededError = require('./LimitExceededError');
+const stream = require('stream')
+const LimitExceededError = require('./LimitExceededError')
 
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
-    super(options);
+    super(options)
+    this.limit = options.limit
+    this.results = 0
   }
 
   _transform(chunk, encoding, callback) {
-
+    this.results += chunk.length
+    if (this.results > this.limit) {
+      return callback(new LimitExceededError())
+    }
+    callback(null, chunk)
   }
 }
 
-module.exports = LimitSizeStream;
+module.exports = LimitSizeStream
